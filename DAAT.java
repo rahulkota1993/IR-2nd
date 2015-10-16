a1f63faa15dfacae320359e3486ddf8969fc8b23
rahulkota1993 <rahulkota1993@gmail.com>
2015-10-16 00:43:04 -0400

    comparisions included
    working

--- DAAT.java
+++ DAAT.java
@@ -1,5 +1,6 @@
  package assignment2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -12,28 +13,36 @@ public class DAAT {
 	String sdaat;
 	HashMap<String, LinkedList<String>> hmdaat;
 	String[] sdaatarray;
+	int compand=0;
+	int compor=0;
 
 	public DAAT(String s,HashMap<String, LinkedList<String>> sl){
 		sdaat=s;
 		hmdaat=sl;
 		sdaatarray=sdaat.split(" ");
-		stringarraycomparator scp=new stringarraycomparator(hmdaat);
-		Arrays.sort(sdaatarray, scp);
-		for(String temp:sdaatarray){
-			System.out.println(temp+" :"+hmdaat.get(temp));
-			System.out.println("size :"+hmdaat.get(temp).size());
+//		stringarraycomparator scp=new stringarraycomparator(hmdaat);
+// 		Arrays.sort(sdaatarray, scp);
 
-		}
 	}
 
 	public LinkedList<String> DAATand(){
+		
 		LinkedList<String> result=new LinkedList<String>();
 		LinkedList<String> tempstore=new LinkedList<String>();
 		int comparisions=0;
-		tempstore=(LinkedList<String>) hmdaat.get(sdaatarray[0]).clone();
-		if(sdaatarray.length==1){
-			result=(LinkedList<String>) tempstore.clone();
+		for(String test:sdaatarray){
+			if(hmdaat.get(test)==null){
+				tempstore.add("terms not found");
+				compand=comparisions;
+				return tempstore;
+			}
 		}
+		stringarraycomparator scp=new stringarraycomparator(hmdaat);
+		Arrays.sort(sdaatarray, scp);
+		
+		
+		tempstore=(LinkedList<String>) hmdaat.get(sdaatarray[0]).clone();
+
 		for(int i=1;i<sdaatarray.length;i++){
 			ListIterator<String> itr1=tempstore.listIterator();
 			LinkedList<String> lister=new LinkedList<String>();
@@ -47,26 +56,22 @@ public class DAAT {
 				tt=itr2.next();				
 				int first=Integer.parseInt(t);
 				int second=Integer.parseInt(tt);
-				if(first==second){
-				
-					result.add(t);
-					
-					
+				if(first==second){				
+					result.add(t);					
 					comparisions=comparisions+1;
-					//System.out.println(t);
-					//System.out.println(" here equal"+comparisions);
+					
 				}
 				else if(first>second){
 					while((first>second)&&(itr2.hasNext()))
 					{
 						comparisions=comparisions+1;
-						//System.out.println("High first Integer number doc:"+second+" comparisions:"+comparisions);
+						
 						tt=itr2.next();
 						second=Integer.parseInt(tt);
 
 					}
 					comparisions=comparisions+1;
-					//System.out.println(comparisions);
+					
 					if(first==second)
 					{
 						result.add(t);
@@ -78,12 +83,12 @@ public class DAAT {
 				else{
 					while((first<second)&& (itr1.hasNext())){
 						comparisions=comparisions+1;
-						//System.out.println("less first integer:"+first);
+						
 						t=itr1.next();
 						first=Integer.parseInt(t);						
 					}					
 					comparisions=comparisions+1;
-					//System.out.println(comparisions);
+					
 					if(first==second){						
 						result.add(t);
 											}			
@@ -96,26 +101,50 @@ public class DAAT {
 			tempstore=(LinkedList<String>) result.clone();
 			
 			if(result.isEmpty()){
-				System.out.println("null");
-				break;
+				tempstore.clear();
+				tempstore.add("terms not found");
+				compand=comparisions;
+				return tempstore;
 			}
 			result.clear();
 
 		}
 		System.out.println("Comparisions:"+comparisions);
+		compand=comparisions;
 		return tempstore;
 	}
 	public LinkedList<String> DAATor(){
 		LinkedList<String> result=new LinkedList<String>();
 		LinkedList<String> tempstore=new LinkedList<String>();
 		int comparisions=0;
-		tempstore=(LinkedList<String>) hmdaat.get(sdaatarray[0]).clone();
-		if(sdaatarray.length==1){
-			result=(LinkedList<String>) tempstore.clone();
+		
+
+		ArrayList<String> al=new ArrayList<String>();
+		for(String test:sdaatarray){
+			if(hmdaat.containsKey(test)){
+				al.add(test);
+			}
 		}
-		for(int i=1;i<sdaatarray.length;i++){
+		if(al.size()==0){
+			tempstore.add("terms not found");
+			compor=comparisions;
+			return tempstore;
+		}
+		String[] daatorarray=new String[al.size()];
+		daatorarray=al.toArray(daatorarray);
+		System.out.println("daatorarraylength :"+daatorarray.length);
+		for(int i=0;i<daatorarray.length;i++){
+			System.out.println("array elements: "+daatorarray[i]);
+		}
+		
+		
+		stringarraycomparator scpor=new stringarraycomparator(hmdaat);
+		Arrays.sort(daatorarray, scpor);
+		
+		tempstore=(LinkedList<String>) hmdaat.get(daatorarray[0]).clone();
+		for(int i=1;i<daatorarray.length;i++){
 			ListIterator<String> itr1=tempstore.listIterator();
-			ListIterator<String> itr2=hmdaat.get(sdaatarray[i]).listIterator();
+			ListIterator<String> itr2=hmdaat.get(daatorarray[i]).listIterator();
 			String t;
 			String tt;
 
@@ -159,23 +188,17 @@ public class DAAT {
 
 				}
 				else{
-					//result.add(tt);
+					
 					while((first<second)&& (itr1.hasNext())){
 						comparisions=comparisions+1;
 						result.add(t);
-						//System.out.println("less first integer:"+first);
+						
 						t=itr1.next();
 						first=Integer.parseInt(t);						
 					}
 
 					comparisions=comparisions+1;
-					//System.out.println(comparisions);
-					//						if(first!=second){
-					//							result.add(tt);
-					//							itr1.previous();
-					//						}else{
-					//							result.add(tt);
-					//						}
+					
 					if(second>first){
 						comparisions++;
 						result.add(t);
@@ -207,7 +230,15 @@ public class DAAT {
 
 		}
 		System.out.println("Comparisions:"+comparisions);
+		compor=comparisions;
 		return tempstore;	
+	}
+	public int comparisonsand(){
+		
+		return compand;
+	}
+	public int comparisionsor(){
+		return compor;
 	}
 }
 
